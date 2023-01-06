6e266326e682407955895ddb82eb1d6b550982e8
Andrii Popov <popov.andrey80@gmail.com>
2023-01-07 01:15:30 +0200

    begining task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +9,12 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Routes>
+        <Route path="/" element={<div>React homework template</div>}></Route>
+      </Routes>
     </div>
   );
 };
